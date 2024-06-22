77d2bdb73c595049bed528e6114f1fceabb92706
Vijay-kumar11 <132987028+Vijay-kumar11@users.noreply.github.com>
2024-06-22 10:01:38 +0530

    Add files via upload

---
+++ reverse number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n, rev=0;
+	cout<<"enter the number";
+	cin>>n;
+	      while(n!=0){
+	      	rev=rev*10 + n%10;
+	      	n/=10;	
+		  }
+		  cout<<"reverse"<<rev<<endl;	  
+}

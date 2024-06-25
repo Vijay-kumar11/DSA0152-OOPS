6d54e0e001dca715923f3873648c02e6f85130aa
Vijay-kumar11 <132987028+Vijay-kumar11@users.noreply.github.com>
2024-06-25 09:49:36 +0530

    Add files via upload

--- reverse number.cpp
+++ reverse number.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main(){
 	int n, rev=0;
-	cout<<"enter the number";
+	cout<<"enter the number: ";
 	cin>>n;
 	      while(n!=0){
 	      	rev=rev*10 + n%10;
 	      	n/=10;	
 		  }
-		  cout<<"reverse"<<rev<<endl;	  
+		  cout<<"reverse number is: "<<rev<<endl;	  
 }

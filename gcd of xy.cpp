eb79a2710bc7b0499d340ac63ccd486b72be1a68
Vijay-kumar11 <132987028+Vijay-kumar11@users.noreply.github.com>
2024-06-25 10:32:56 +0530

    Add files via upload

---
+++ gcd of xy.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b){
+	while(b!=0){
+	int temp = b;
+	b=a%b;
+	a = temp;	
+	}
+	return a;
+}
+int main(){
+	int n1, n2;
+	cout<< "num 1 = ";
+	cin>>n1;
+	cout << "num 2= ";
+	cin >> n2;
+	cout << "GCD of two numbers is = " << gcd(n1, n2);
+}

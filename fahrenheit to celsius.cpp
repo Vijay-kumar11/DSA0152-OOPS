5b7c4a92783f2cf1ae8a2b9747acf138798df48d
Vijay-kumar11 <132987028+Vijay-kumar11@users.noreply.github.com>
2024-06-25 10:17:35 +0530

    Add files via upload

---
+++ fahrenheit to celsius.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using  namespace std;
+int main() {
+	float celsius,fahrenheit;
+	cout<<"enter the temperature in celsius";
+	cin>>celsius;
+	fahrenheit=(celsius * 9.0/5.0)+32.0;
+	cout<<"the temperature in fahrenhet is:"<<fahrenheit<<"f"<<endl;
+	return 0;
+} 

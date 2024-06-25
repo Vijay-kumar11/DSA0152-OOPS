b471c19508c50e1d4653def4654b9855f7f0dc1e
Vijay-kumar11 <132987028+Vijay-kumar11@users.noreply.github.com>
2024-06-25 10:05:09 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i, n, fact=1;
+	cout << "enter the number: ";
+	cin >> n;
+	for (i=1; i<=n; i++){
+	if (fact = fact * i);
+	cout << "factorial of number is: " << fact << endl;
+	}
+	return 0;
+}
+
